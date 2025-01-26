2dbf37c4c040a6e5c6d15d0230914c52c01f1751
edmundrotimi <edmundrotimi@gmail.com>
2025-01-26 02:09:29 +0200

    update unfold context

--- core/project/settings/unfold/context.py
+++ core/project/settings/unfold/context.py
@@ -1,13 +1,22 @@
+from django.db.models import Max, Min, Sum
+
+from core.fuel.models import FuelPrices
+from core.project.settings import ADMIN_PATH
+
+
 def dashboard_callback(request, context):
     """
     Callback to prepare custom variables for index template which is used as dashboard
     template. It can be overridden in application by creating custom admin/index.html.
     """
 
-    # context.update({
+    context.update({
+        'fuel_prices':
+            FuelPrices.objects.all().annotate(
+                max_price=Max('retail_price'), min_price=Min('retail_price'), total_price=Sum('retail_price')
+            ),
+        'admin_path':
+            ADMIN_PATH
+    },)
 
-    # },)
-
-    # return context
-
-    pass
+    return context
